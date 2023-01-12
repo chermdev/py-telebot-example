ead4808cda7666bcc97bc2c3fac75406ac9546b0
ChermDev <carlos_e8246@hotmail.com>
2023-01-11 23:53:23 -0600

    Add bot and requirements

---
+++ bot.py
@@ -0,0 +1,26 @@
+import os
+import telebot
+from dotenv import load_dotenv
+
+
+load_dotenv()
+API_KEY = os.getenv('API_KEY')
+bot = telebot.TeleBot(API_KEY)
+
+
+@bot.message_handler(commands=["start"])
+def start(message):
+    bot.send_message(message.chat.id, "Hello! Send me:\n/hello\n/greet")
+
+
+@bot.message_handler(commands=["greet"])
+def greet(message):
+    bot.reply_to(message, "Hey! How you doing?")
+
+
+@bot.message_handler(commands=["hello"])
+def hello(message):
+    bot.send_message(message.chat.id, "Hello!")
+
+
+bot.polling()
