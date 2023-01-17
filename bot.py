8ad347cd61f054812cfaad63d97ea2f5c8323c5b
ChermDev <carlos_e8246@hotmail.com>
2023-01-16 23:08:50 -0600

    Add logging

--- bot.py
+++ bot.py
@@ -1,9 +1,22 @@
 import os
 import telebot
 from dotenv import load_dotenv
+import logging
 
 
 load_dotenv()
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        logging.FileHandler("bot.log"),
+        logging.StreamHandler()
+    ]
+)
+
+
 API_KEY = os.getenv('API_KEY') if os.getenv(
     'API_KEY') else os.environ.get('API_KEY')
 bot = telebot.TeleBot(API_KEY)
@@ -11,16 +24,19 @@ bot = telebot.TeleBot(API_KEY)
 
 @bot.message_handler(commands=["start"])
 def start(message):
+    logging.info("User entered /start Hello! Send me:\n/hello\n/greet")
     bot.send_message(message.chat.id, "Hello! Send me:\n/hello\n/greet")
 
 
 @bot.message_handler(commands=["greet"])
 def greet(message):
+    logging.info("User entered /great Hey! How you doing?")
     bot.reply_to(message, "Hey! How you doing?")
 
 
 @bot.message_handler(commands=["hello"])
 def hello(message):
+    logging.info("User entered /hello Hello!")
     bot.send_message(message.chat.id, "Hello!")
 
 
